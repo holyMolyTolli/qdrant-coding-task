c03cbfbd301c59e8b209e5d90571c911f682c5d6
Kirstin <taukiki@aol.com>
2025-08-10 13:21:03 +0200

    Remove commented dataset name from configuration

--- app/config.py
+++ app/config.py
@@ -9,7 +9,6 @@ SHARD_NUMBER = REPLICATION_FACTOR * 2
 
 MAX_DOCUMENTS = BATCH_SIZE * 2
 DATASET_NAME = "squad"
-# DATASET_NAME = "Qdrant/arxiv-titles-instructorxl-embeddings"
 
 DENSE_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
 SPARSE_MODEL_NAME = "Qdrant/bm25"
